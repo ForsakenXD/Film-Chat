0c5d7f6ce41c0e67bfb7313ae67cab7944b8244f
ForsakenXD <41171766+ForsakenXD@users.noreply.github.com>
2019-05-16 18:55:09 +0300

    finished popular movies section

--- src/Components/popular-movies.js
+++ src/Components/popular-movies.js
@@ -1,6 +1,5 @@
 import React from 'react'
-
-
+import {Button} from 'react-bootstrap'
 
 
 
@@ -15,16 +14,13 @@ class PopularMovies extends React.Component{
   constructor(){
     super()
     this.state={}
-
     this.show()
-
   }
 
 
 
 
   show(){
-    console.log('eyy bo')
     const urlString = "https://api.themoviedb.org/3/movie/popular?api_key=1adbe5b9d80d1dc5e9cd90c2e0c31900&language=en-US&page=1"
     fetch(urlString)
       .then(function(response) {return response.json(); })
@@ -33,18 +29,22 @@ class PopularMovies extends React.Component{
         const background_poster = []
         let counter = 1
         data.results.forEach((movie) => {
-          if(counter<=10)
+          console.log(movie)
+          if(counter<=12) //(how many movies will be show on the popular movies list)
           {
-            console.log(movie)
-            let url2 = "https://image.tmdb.org/t/p/w200/" + movie.poster_path
-            const mov = <figure class="imghvr-blur" style={{backgroundColor: 'transparent'}}>
+            const url2 = "https://image.tmdb.org/t/p/w200/" + movie.poster_path
+            const url1 = "https://www.themoviedb.org/movie/" + movie.id
+            const mov = <figure className="imghvr-blur" style={{backgroundColor: 'transparent'}} key={movie.original_title}>
                             <img alt={(movie.original_title)} src={url2}   key={(movie.original_title)} className="poster"/>
                             <figcaption style={{backgroundColor:"transparent"},gradient}>
                               <h3 className="ih-fade-down ih-delay-sm" style={{fontSize:'25px'}}>{movie.original_title}</h3>
-
-
+                                  <div className="d-flex flex-row" >
+                                  <Button onClick={()=> window.open(url1, "_blank")} variant="danger sm" style={{marginRight:'1em',backgroundColor:'crimson',borderColor:'crimson'}}>View</Button>
+                                  <Button variant="success"   style={{backgroundColor:'black',borderColor:'black'}}>Chat!</Button>
+                                  </div>
+                                  <h6>Release Date:{movie.release_date}</h6>
+                                  <h6>Rating:{movie.vote_average}/10</h6>
                             </figcaption>
-                            <a href="#"></a>
                         </figure>
             background_poster.push(mov)
             counter++
@@ -53,14 +53,6 @@ class PopularMovies extends React.Component{
         this.setState({PopularRow:background_poster})
       }.bind(this))
   }
-
-
-
-
-
-
-
-
 
 
 
